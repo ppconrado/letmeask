1afe83be3903719e6a46968338b8bdd86edcbb3b
ppconrado <ppconrado@yahoo.com.br>
2021-06-22 03:38:20 -0300

    routes + google auth firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useHistory } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
@@ -5,21 +7,33 @@ import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../componets/Button';
 
 import '../styles/auth.scss';
+import { auth, firebase } from '../services/firebase';
 
 // webpack (snowpack, vite, ...)
 
 export function Home() {
+
+    const history = useHistory();
+
+    function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result)
+            history.push('/rooms/new');
+        })
+    }
+
     return(
         <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="ilustração simbolizando perguntas e respostas" />
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
-                <p>Tire as duvidas da sua audiência em tempo real</p>
+                <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="logo do Google" />
                         Crie sua sala com o Google
                     </button>
