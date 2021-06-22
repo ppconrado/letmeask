880af02d0ffa9139e97c9ec296322532df5a0ca1
ppconrado <ppconrado@yahoo.com.br>
2021-06-22 04:53:57 -0300

    react context added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { createContext, useState } from 'react';
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home"
 import { NewRoom } from "./pages/NewRoom";
 
-//import { NewRoom } from "./pages/NewRoom";
+export const TestContext = createContext({} as any)
 
 function App() {
+const [value, setValue] = useState('Teste');
+
   return (
     <BrowserRouter>
-    <Route path="/" exact component={Home}/>
-    <Route path="/rooms/new" component={NewRoom}/>
+    <TestContext.Provider value={{ value, setValue }}>
+      <Route path="/" exact component={Home}/>
+      <Route path="/rooms/new" component={NewRoom}/>
+      </TestContext.Provider>
     </BrowserRouter>
   );
 }
