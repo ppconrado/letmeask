7be0f42d005c0eca52a64ff1f65cba78ebd85bca
ppconrado <ppconrado@yahoo.com.br>
2021-06-25 05:32:26 -0300

    questions structure(HTML + CSS)

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg";
 
 import { Button } from "../componets/Button";
+import { Question } from "../componets/Question";
 import { RoomCode } from "../componets/RoomCode";
 import { useAuth } from "../hooks/useAuth";
 import { database } from "../services/firebase";
@@ -22,7 +23,7 @@ type FirebaseQuestions = Record<
   }
 >;
 
-type Question = {
+type QuestionType = {
   id: string;
   author: {
     name: string;
@@ -41,7 +42,7 @@ export function Room() {
   const { user } = useAuth();
   const params = useParams<RoomParams>();
   const [newQuestion, setNewQuestion] = useState("");
-  const [questions, setQuestions] = useState<Question[]>([]);
+  const [questions, setQuestions] = useState<QuestionType[]>([]);
   const [title, setTitle] = useState("");
 
   const roomId = params.id;
@@ -130,8 +131,20 @@ export function Room() {
           </div>
         </form>
 
-        {JSON.stringify(questions)}
+        <div className="question-list">
+          {questions.map((question) => {
+            return (
+              <Question
+                key={question.id}
+                content={question.content}
+                author={question.author}
+              />
+            );
+          })}
+        </div>
       </main>
     </div>
   );
 }
+
+// Algoritmo de reconciliação
